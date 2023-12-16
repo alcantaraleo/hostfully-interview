5c15d2edcf9c8db208102cb1fa08e5e5663cff3b
leonardo.alcantara <leonardo.alcantara@ifood.com.br>
2023-12-16 18:37:11 -0300

    Initial commit with base structure and config

---
+++ src/main/java/com/hostfully/HostfullyTechnicalInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.hostfully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostfullyTechnicalInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostfullyTechnicalInterviewApplication.class, args);
+	}
+
+}
